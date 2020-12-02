3b4d597b178744b54b6f09544a4af3b3a9062ecf
sofinms <sofinms@gmail.com>
2020-12-02 09:00:00 +0400

    trouble example

--- new_bot_test.rb
+++ new_bot_test.rb
@@ -18,26 +18,43 @@
     },
     {
         'id' => 5,
-        'ings' => [0,0,2,0]
+        'ings' => [0,-2,2,0]
+    },
+    {
+        'id' => 6,
+        'ings' => [1,1,1,-1]
+    },
+    {
+        'id' => 7,
+        'ings' => [3,0,1,-1]
+    },
+    {
+        'id' => 8,
+        'ings' => [0,-3,0,2]
+    },
+    {
+        'id' => 9,
+        'ings' => [-3,3,0,0]
     },
     {
         'id' => 10,
-        'ings' => [0,0,1,-1]
-    },
-    {
-        'id' => 11,
-        'ings' => [0,-1,-1,2]
+        'ings' => [1,-3,1,1]
     }
 ]
 
-brew = [0, 0, -2, -2]
+brew = [0, -2, -1, -1]
 my_inv = [3, 0, 0, 0]
 @recursion_level = 0
 
-def find_optimal_paths delta_inventory, history
+def find_optimal_paths delta_inventory, history, depricated_ings
 	@recursion_level += 1
 	tabs = (1..@recursion_level).to_a.map{|_e| "\s"}.join
-	STDERR.puts tabs + "Find_path for #{delta_inventory} --\n"
+	STDERR.puts tabs + "@#{@recursion_level}\n"
+	if @recursion_level > 5
+		@recursion_level -= 1
+		return nil
+	end
+	
 	need_ing = delta_inventory.index { |ing| ing < 0 }
 	if need_ing.nil?
 		info = {
@@ -45,11 +62,14 @@ def find_optimal_paths delta_inventory, history
 			'ingredients' => delta_inventory,
 			'history' => history.map{|x| x.clone}
 		}
-		STDERR.puts tabs + "#{info}\n"
+		# STDERR.puts tabs + "#{info}\n"
 		@recursion_level -= 1
 		return info
 	end
-	needed_spells = get_needed_spells need_ing
+	needed_spells = get_needed_spells need_ing, depricated_ings
+	new_depricated_ings = depricated_ings.map{|x| x}
+	new_depricated_ings.push need_ing
+	STDERR.puts tabs + "needed_spells = #{needed_spells.map{|_e| _e['id']}}\n"
 	all_paths = []
 	needed_spells.each do |needed_spell|
 		new_history = history.map{|x| x.clone}
@@ -61,7 +81,8 @@ def find_optimal_paths delta_inventory, history
 		negative_ings_values = delta_inventory.map { |x| x < 0 ? x : 0 }
 		positive_ings = current_delta_inventory.each_with_index.map { |x,i| need_ing == i || x < 0 ? 0 : x }
 		spell_positive_ings = needed_spell["ings"].map { |x| x > 0 ? x : 0 }
-		result_info = find_optimal_paths(get_delta(needed_spell["ings"].map { |x| x > 0 ? 0 : x }, positive_ings), new_history.map{|x| x.clone})
+		STDERR.puts tabs + "Z = #{needed_spell['id']}\n"
+		result_info = find_optimal_paths(get_delta(needed_spell["ings"].map { |x| x > 0 ? 0 : x }, positive_ings), new_history.map{|x| x.clone}, new_depricated_ings)
 		next if result_info.nil?
 
 		# STDERR.puts tabs + "result_info['ingredients'] = #{result_info["ingredients"]}\n"
@@ -79,7 +100,7 @@ def find_optimal_paths delta_inventory, history
 	all_paths.each do |path_info|
 		#STDERR.puts tabs + "path_info['history'] = #{path_info['history']}"
 		#STDERR.puts tabs + "path_info['ingredients'] = #{path_info['ingredients']}"
-		result_info = find_optimal_paths path_info["ingredients"], path_info['history'].map{|x| x.clone}
+		result_info = find_optimal_paths path_info["ingredients"], path_info['history'].map{|x| x.clone}, depricated_ings
 		next if result_info.nil?
 		result_path = path_info["path"] + result_info["path"]
 		if optimal_path_info.nil? || optimal_path_info["path"].count > result_path.count
@@ -90,7 +111,10 @@ def find_optimal_paths delta_inventory, history
 			}
 		end
 	end
-	STDERR.puts tabs + "#{optimal_path_info}\n"
+	# STDERR.puts tabs + "#{optimal_path_info}\n"
+	if optimal_path_info
+		STDERR.puts tabs + "optimal_path_info = #{optimal_path_info['path'].map{|_e| _e['id']}}\n"
+	end
 	@recursion_level -= 1
 	return optimal_path_info
 end
@@ -109,10 +133,19 @@ def get_delta *args
 	delta
 end
 
-def get_needed_spells idx
+def get_needed_spells idx, depricated_ings
 	variants = []
 	@spells.each do |spell|
 		if spell["ings"][idx] > 0
+			negative_ings = []
+			spell["ings"].each_with_index do |ing, idx|
+				if ing < 0
+					negative_ings.push idx
+				end
+			end 
+			if depricated_ings.count > (depricated_ings - negative_ings).count
+				# next
+			end
 			variants.push(spell)
 		end
 	end
@@ -120,5 +153,5 @@ def get_needed_spells idx
 end
 
 delta = get_delta(my_inv, brew)
-idx = find_optimal_paths(delta, [delta])
+idx = find_optimal_paths(delta, [delta], [])
 p idx
